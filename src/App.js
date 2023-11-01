106fb1bae7e213cf5c918758439070e9bb77b484
Agbesi Amenyo <56748821+agbesi-amenyo@users.noreply.github.com>
2023-11-01 19:01:54 +0100

    first commit
    created nav bar, home page and added use state hook to manage the name and age

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Home from './Home';
+import Navbar from './Navbar'
+
+function App() {
+
+  const title ="Welcome to the new blog"
+  return (
+    <div className="App">
+      <Navbar/>
+     <div className="content">
+       <Home/>
+     </div>
+    </div>
+  );
+}
+
+export default App;
