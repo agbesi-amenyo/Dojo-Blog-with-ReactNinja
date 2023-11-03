3060686173ec400d2dc3289d1b3d6839c3d10fe9
Agbesi Amenyo <56748821+agbesi-amenyo@users.noreply.github.com>
2023-11-03 17:01:12 +0100

    made fetch requests

--- src/Home.js
+++ src/Home.js
@@ -3,22 +3,27 @@ import BlogList from "./BlogList";
 
 const Home = () => {
 const [blogs, setBlogs] = useState(null)
+const [isLoading, setIsLoading] = useState(true)
 
 const handleDelete = (id) => {
     const newBlogs = blogs.filter(blogData => blogData.id !== id)
     setBlogs(newBlogs)
 }
 useEffect(()=>{
-   fetch(" http://localhost:8000/blogs").then(res =>{
-      return  res.json()
-   })
-   .then((data) => {
-    setBlogs(data)
-   })
+  setTimeout(()=>{
+    fetch(" http://localhost:8000/blogs").then(res =>{
+        return  res.json()
+     })
+     .then((data) => {
+      setBlogs(data)
+      setIsLoading(false)
+     })
+  },1000)
 }, [])
 
     return ( 
         <div className="home">
+        {isLoading && <div>Loding...</div>}
          {blogs && <BlogList blogs ={blogs}  title ="All Blogs!"  handleDelete={handleDelete}/>}
          {/* <BlogList blogs ={blogs.filter((blog) =>  blog.author === "mario")}  title ="Mario's Blogs"  handleDelete={handleDelete}/> */}
         </div>
