94fa898e27b0b0a8f94448d684ee758db5a1f46e
Agbesi Amenyo <56748821+agbesi-amenyo@users.noreply.github.com>
2023-11-04 17:06:59 +0100

    error message added

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ import BlogList from "./BlogList";
 const Home = () => {
 const [blogs, setBlogs] = useState(null)
 const [isLoading, setIsLoading] = useState(true)
+const [error,setError]= useState(null)
 
 const handleDelete = (id) => {
     const newBlogs = blogs.filter(blogData => blogData.id !== id)
@@ -11,18 +12,28 @@ const handleDelete = (id) => {
 }
 useEffect(()=>{
   setTimeout(()=>{
-    fetch(" http://localhost:8000/blogs").then(res =>{
-        return  res.json()
-     })
-     .then((data) => {
-      setBlogs(data)
-      setIsLoading(false)
-     })
+        fetch(" http://localhost:8000/blogs")
+        .then(res =>{
+            if(!res.ok){
+                throw Error("could not fetch the data for that resource")
+            }
+            return  res.json()
+        })
+        .then((data) => {
+        setBlogs(data)
+        setIsLoading(false)
+        setError(null)
+        })
+        .catch(err => {
+            setIsLoading(false)
+            setError(err.message)
+        })
   },1000)
 }, [])
 
     return ( 
         <div className="home">
+            {error && <div>{error}</div>}
         {isLoading && <div>Loding...</div>}
          {blogs && <BlogList blogs ={blogs}  title ="All Blogs!"  handleDelete={handleDelete}/>}
          {/* <BlogList blogs ={blogs.filter((blog) =>  blog.author === "mario")}  title ="Mario's Blogs"  handleDelete={handleDelete}/> */}
