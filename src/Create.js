d426975210b276b3931a953fe4635b285123b094
Agbesi Amenyo <56748821+agbesi-amenyo@users.noreply.github.com>
2023-11-06 13:59:34 +0100

    form complete, submit button working

--- src/Create.js
+++ src/Create.js
@@ -1,13 +1,34 @@
 import { useState } from "react";
+import { useHistory } from "react-router";
 
 const Create = () => {
     const [title, setTitle] =useState('');
     const [body, setBody] =useState('');
-    const [author,setAuthor] =useState('');
+    const [author,setAuthor] =useState('mario');
+
+    const [isPending,setIsPending]= useState(false)
+    const history = useHistory()
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        const blog ={title, body, author}
+        setIsPending(true)
+        fetch('http://localhost:8000/blogs',{
+        
+        method : 'POST',
+        headers :{"Content-Type":"application/json"},
+        body: JSON.stringify(blog)
+        }).then(()=>{
+            console.log('New blog added');
+            setIsPending(false);
+            history.go(-1);
+        })
+
+    }
     return ( 
         <div className="create">
             <h2>Add a new Blog</h2>
-            <form>
+            <form onSubmit= {handleSubmit}>
                 <label>Blog title: </label>
                 <input 
                 type="text"
@@ -23,16 +44,15 @@ const Create = () => {
                 ></textarea>
                 <label>Blog author: </label>
                 <select
+                required
                 value = {author}
                 onChange ={(e)=>setAuthor(e.target.value)}
                 >
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
-                <button>Add Blog</button>    
-                <p>{title}</p>        
-                <p>{body}</p>  
-                <p>{author}</p> 
+                {!isPending && <button>Add Blog</button> }
+                {isPending && <button>Adding new blog...</button> }
             </form>
         </div>
      );
